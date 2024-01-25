4ad97f80d191a07a0d4701ed1bb117d1c54bcf63
Heinich Porro <heinich.porro@unilim.fr>
2024-01-25 11:49:30 +0100

    update the visuals, add a model

--- main/scrpt.js
+++ main/scrpt.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { ArcballControls } from 'three/addons/controls/ArcballControls.js';
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
 // Sphere
 
@@ -36,6 +38,29 @@ loader.load(
   }
 )
 
+loader.load(
+  './assets/man.stl',
+  function (geometry) {
+    const material = new THREE.MeshPhysicalMaterial({
+      color: 0xeb34a8,
+      roughness: 0.5,
+      metalness: 0.5,
+      reflectivity: 0.5,
+    });
+    geometry.translate(10, 0, 0)
+    const mesh = new THREE.Mesh(geometry, material)
+    // mesh.position.set(0, 0, 0)
+    mesh.scale.set(0.5, 0.5, 0.5);
+    scene.add(mesh)
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {
+    console.log(error)
+  }
+)
+
 const geometry = new THREE.PlaneGeometry(30, 30);
 geometry.translate(0, 0, -1)
 const material = new THREE.MeshPhysicalMaterial({
@@ -48,26 +73,13 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // Light
-const light1 = new THREE.PointLight(0xffffff, 1, 100);
-light1.position.set(0, 10, 10);
-const light2 = new THREE.PointLight(0xffffff, 1, 100);
-light2.position.set(10, 0, 10);
-const light3 = new THREE.PointLight(0xffffff, 1, 100);
-light3.position.set(10, 10, 0);
-const light4 = new THREE.PointLight(0xffffff, 1, 100);
-light3.position.set(0, 0, -20);
-const light5 = new THREE.PointLight(0xffffff, 1, 100);
-light5.position.set(0, 20, -15);
-scene.add(light1);
-scene.add(light2);
-scene.add(light3);
-scene.add(light4);
 
-const aLight = new THREE.AmbientLight(0x151515);
-scene.add(aLight);
+const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 2.5);
+light.position.set(0.5, 1, 0.75);
+scene.add(light);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.z = 20;
 scene.add(camera);
 
@@ -76,7 +88,6 @@ const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
-renderer.setClearColor(0xeeeeee);
 
 // Resize 
 window.addEventListener('resize', () => {
@@ -88,12 +99,10 @@ window.addEventListener('resize', () => {
 
 
 // Animation Loop
-const controls = new OrbitControls(camera, canvas);
+const controls = new ArcballControls(camera, canvas);
 controls.enableDamping = true;
 let angle = 0;
 const loop = () => {
-
-  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop);
 }
